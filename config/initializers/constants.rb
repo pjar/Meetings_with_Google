721a0508970800f4b244168616a777315c8385a7
Jarek Plonski <jaroslaw.plonski@gmail.com>
2011-07-08 21:33:40 +0100

    maintenance works

--- config/initializers/constants.rb
+++
@@ -1,6 +0,0 @@
-# constants have to be defined for different Google Accounts - TODO - organize it in different way
-
-USER_NAME = "calendarapplicationtest@gmail.com"
-PASSWORD = "basicpassword999"
-
-
